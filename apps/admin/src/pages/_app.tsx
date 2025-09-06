d36bf8e8e1c056f414dd9df9f33fa618351fc0ac
BByungs <byungjin0120@gmail.com>
2025-09-06 21:36:54 +0900

    📦 chore(admin): lint 적용

--- apps/admin/src/pages/_app.tsx
+++ apps/admin/src/pages/_app.tsx
@@ -1,5 +1,5 @@
-import type { AppProps } from "next/app";
-import "../styles/globals.css";
+import type { AppProps } from 'next/app';
+import '../styles/globals.css';
 
 export default function AdminApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
