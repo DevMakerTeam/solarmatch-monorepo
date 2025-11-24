9d465671f12d7c304c539106f093f73042a0c758
BYEONGJIN <byeongjin@BYEONGJINui-MacBookAir.local>
2025-11-24 23:04:32 +0900

    ✨ feat(web): 필요한 api 세팅

--- apps/web/src/api/image/ImageApi.mutation.ts
+++ apps/web/src/api/image/ImageApi.mutation.ts
@@ -5,13 +5,26 @@ import { useMutation } from "@tanstack/react-query";
 export const IMAGE_API_MUTATION_KEY = {
   PARTNER_LOGO: (params?: Parameter<typeof imageApi.partnerLogo>) =>
     ["partner-logo", params].filter(isNotNullish),
+  IMAGE_UPLOAD: (params?: Parameter<typeof imageApi.imageUpload>) =>
+    ["image-upload", params].filter(isNotNullish),
 };
 
+// 파트너 로고 이미지 업로드
 export const usePartnerLogoMutation = (
   params?: UseMutationParams<typeof imageApi.partnerLogo>
 ) => {
   return useMutation({
     mutationFn: imageApi.partnerLogo,
+    ...params?.options,
+  });
+};
+
+// 일반 이미지 업로드 (다중)
+export const useImageUploadMutation = (
+  params?: UseMutationParams<typeof imageApi.imageUpload>
+) => {
+  return useMutation({
+    mutationFn: imageApi.imageUpload,
     ...params?.options,
   });
 };
